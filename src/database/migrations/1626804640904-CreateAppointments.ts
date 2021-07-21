e68a00a3d3adea5d53a8736920232aaa6f6559a1
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-07-21 17:48:11 -0300

    fix: added uuid generator v4

--- src/database/migrations/1626804640904-CreateAppointments.ts
+++ src/database/migrations/1626804640904-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1626804640904
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
 
           {
