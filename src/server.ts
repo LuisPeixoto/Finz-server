d492a6aa65ed1a0dd47a822ad4348743552509ea
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-07-21 17:59:55 -0300

    chore: add reflect-metadata

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import 'reflect-metadata'
+
 import express from 'express'
-import router from './routes'
+import routes from './routes'
 
 import './database'
 
 const app = express()
 
-app.use(router)
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('ok')
