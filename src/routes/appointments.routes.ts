cc39833518c3b4b237fc72b89d2dc5d62bf8e94a
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-07-21 18:08:32 -0300

    feat:  support GET/POST methods

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { parseISO } from 'date-fns'
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+const appointmentsRouter = Router()
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  const appoitments = await appointmentsRepository.find()
+
+  return response.json(appoitments)
+})
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body
+
+    const parsedDate = parseISO(date)
+
+    const createAppointment = new CreateAppointmentService()
+
+    const appoitment = await createAppointment.execute({
+      date: parsedDate,
+      provider,
+    })
+
+    return response.json(appoitment)
+  } catch (error) {
+    console.log(request.body)
+    return response.status(400).json({ error: error.message })
+  }
+})
+
+export default appointmentsRouter
