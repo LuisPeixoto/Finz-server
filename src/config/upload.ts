721dbd86ad110be652dc0b9f297349cf09883fe1
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-07-28 16:17:02 -0300

    feat: upload avatar

---
+++ src/config/upload.ts
@@ -0,0 +1,15 @@
+import multer from 'multer'
+import crypto from 'crypto'
+import path from 'path'
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename(request, file, callback) {
+      const filehash = crypto.randomBytes(10).toString('hex')
+      const filename = `${filehash}-${file.originalname}`
+
+      return callback(null, filename)
+    },
+  }),
+}
